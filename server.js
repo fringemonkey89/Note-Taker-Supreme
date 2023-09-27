12bdfaaad16be88ea5f9b1d0e8a5e2e1cb7ff626
fringemonkey89 <markomajeticjr@gmail.com>
2023-09-27 14:13:05 -0400

    added delete function and fixed code in server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
 const fs = require('fs');
 let notes = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
@@ -11,21 +10,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-
-
-//middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//used to serve the static files from the public directory
-
-app.use(express.static('public'));
-
 //defines the route for the root url using app.get(), it responds by sending the index file from the public directory 
 //using res.sendFile();
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
 
 //defines the route for the /notes url using app.get(), it responds by sending the notes file from the public directory 
 //using res.sendFile();
@@ -35,20 +25,25 @@ app.get('/notes', (req, res) =>
 //defines the route for the /api/notes url using app.get(), it responds by sending the db JSON file as the response  
 //using res.json();. used to retrieve notes from the JSON database
 app.get('/api/notes', (req, res) => 
-res.json(db));
+res.json(notes));
 
-//defines a route for the /api/notes url using app.post. it expects the incoming requests with a JSON body containing
-// title and text properties
+//defines a route for the /api/notes url using app.post
 app.post('/api/notes', (req, res) => {
-    const {title, text} = req.body;
-    //creates a new note object using values from the request body
-    const newNote = {title, text};
-    //new note is then pushed to the db.json file using fs.writeFileSync
-    db.push(newNote);
-    fs.writeFileSync("db/db.json", JSON.stringify(db))
-    //responds by sending the updated db JSON object as the response using res.json
-    res.json(db);
+    req.body.id = uniqid();
+    notes.push(req.body);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2))
+    res.json(notes);
   })
+
+  app.delete("/api/notes/:id", (req, res) => {
+    notes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notes, null, 2)
+    );
+    res.json(notes);
+});
+
     
   app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
